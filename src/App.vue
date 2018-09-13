c71fda643b3b85bff0f8dd4c0d3fb8bc8b1c8cea
Tracy.yang <tracy.yang@movit-tech.com>
2018-09-13 16:40:07 +0800

    构建项目

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body{
+  padding:0;
+  margin: 0;
+}
+#app {
+  font-family:'microsoft yahei', 'Avenir', Helvetica, Arial, sans-serif;
+  padding:0;
+  margin: 0;
+}
+</style>
---
+++ src/views/userList/userList.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="wrapper">
+        <div class="header">
+            <img src="../../assets/logo.png" alt="">
+            <span>张三</span>
+        </div>
+        <Input v-model="value" placeholder="输入您要查询的姓名" />
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper{
+    padding:18px;
+    background:#2E3238;
+
+    .header{
+        img{
+            width: 40px;
+            height: 40px;
+        }
+        span{
+            display: inline-block;
+            font-weight: 400;
+            width: 156px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            word-wrap: normal;
+            color: #fff;
+            font-size: 18px;
+            vertical-align: top;
+            line-height: 31px;
+            text-align: left;
+        }
+    }
+}
+</style>
+
+
+
+
